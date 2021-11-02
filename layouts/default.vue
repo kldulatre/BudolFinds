f18abe8ad26eda2ed133ac5e65660d699bfbe35d
Kenjie Lloyd Dulatre <kdulatre@up.edu.ph>
2021-11-02 20:32:37 +0800

    communities ui

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="min-h-screen bg-gray-50">
+  <div class="min-h-screen bg-gray-100">
     <NavBar />
-    <div class="container mx-auto flex justify-center items-start">
+    <div class="container mx-auto flex justify-center items-start bg-white min-h-screen">
       <SideBarLeft class="w-4/12 hidden md:block"/>
       <Nuxt class="w-full md:w-8/12" />
     </div>
---
+++ pages/communities.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="p-5 space-y-2">
+    <div class="flex justify-between block pb-2">
+      <span class="text-3xl font-medium">Communities</span>
+      <button class="bg-green-300 p-3 rounded-full text-xs">
+        Create a Community
+      </button>
+    </div>
+    <div class="space-y-6 p-6 border rounded-lg">
+      <div class="flex justify-between">
+        <span class="text-xl font-medium">My Communities</span>
+        <NuxtLink to="/"
+          ><span class="text-sm text-blue-600 font-semibold"
+            >See all</span
+          ></NuxtLink
+        >
+      </div>
+      <ul class="space-y-6">
+        <li class="flex items-center space-x-2">
+          <img
+            class="inline-block h-10 w-10 rounded-full"
+            src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png"
+            alt=""
+          />
+          <div class="">
+            <span class="block">Philippines</span>
+            <span class="block text-xs text-gray-500"
+              >Archipelagic Country found on the western part of the Pacific
+              Ocean</span
+            >
+          </div>
+        </li>
+        <li class="flex items-center space-x-2">
+          <img
+            class="inline-block h-10 w-10 rounded-full"
+            src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png"
+            alt=""
+          />
+          <div class="">
+            <span class="block">Philippines</span>
+            <span class="block text-xs text-gray-500"
+              >Archipelagic Country found on the western part of the Pacific
+              Ocean</span
+            >
+          </div>
+        </li>
+        <li class="flex items-center space-x-2">
+          <img
+            class="inline-block h-10 w-10 rounded-full"
+            src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png"
+            alt=""
+          />
+          <div class="">
+            <span class="block">Philippines</span>
+            <span class="block text-xs text-gray-500"
+              >Archipelagic Country found on the western part of the Pacific
+              Ocean</span
+            >
+          </div>
+        </li>
+        <li class="flex items-center space-x-2">
+          <img
+            class="inline-block h-10 w-10 rounded-full"
+            src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png"
+            alt=""
+          />
+          <div class="">
+            <span class="block">Philippines</span>
+            <span class="block text-xs text-gray-500"
+              >Archipelagic Country found on the western part of the Pacific
+              Ocean</span
+            >
+          </div>
+        </li>
+        <li class="flex items-center space-x-2">
+          <img
+            class="inline-block h-10 w-10 rounded-full"
+            src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png"
+            alt=""
+          />
+          <div class="">
+            <span class="block">Philippines</span>
+            <span class="block text-xs text-gray-500"
+              >Archipelagic Country found on the western part of the Pacific
+              Ocean</span
+            >
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="space-y-6 p-6 border rounded-lg">
+      <div class="flex justify-between">
+        <span class="text-xl font-medium">Trending Communities</span>
+        <NuxtLink to="/"
+          ><span class="text-sm text-blue-600 font-semibold"
+            >See all</span
+          ></NuxtLink
+        >
+      </div>
+      <ul class="space-y-6">
+        <li class="flex items-center space-x-2">
+          <img
+            class="inline-block h-10 w-10 rounded-full"
+            src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png"
+            alt=""
+          />
+          <div class="flex-grow">
+            <span class="block">Philippines</span>
+            <span class="block text-xs text-gray-500"
+              >Archipelagic Country found on the western part of the Pacific
+              Ocean</span
+            >
+          </div>
+          <span class="text-xs bg-green-300 rounded-full px-4 py-2">Join</span>
+        </li>
+        <li class="flex items-center space-x-2">
+          <img
+            class="inline-block h-10 w-10 rounded-full"
+            src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png"
+            alt=""
+          />
+          <div class="flex-grow">
+            <span class="block">Philippines</span>
+            <span class="block text-xs text-gray-500"
+              >Archipelagic Country found on the western part of the Pacific
+              Ocean Ocean Country found on the western part of the</span
+            >
+          </div>
+          <span class="text-xs bg-green-300 rounded-full px-4 py-2">Join</span>
+        </li>
+        <li class="flex items-center space-x-2">
+          <img
+            class="inline-block h-10 w-10 rounded-full"
+            src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png"
+            alt=""
+          />
+          <div class="flex-grow">
+            <span class="block">Philippines</span>
+            <span class="block text-xs text-gray-500"
+              >Archipelagic Country found on the western part of the Pacific
+              Ocean</span
+            >
+          </div>
+          <span class="text-xs bg-green-300 rounded-full px-4 py-2">Join</span>
+        </li>
+        <li class="flex items-center space-x-2">
+          <img
+            class="inline-block h-10 w-10 rounded-full"
+            src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png"
+            alt=""
+          />
+          <div class="flex-grow">
+            <span class="block">Philippines</span>
+            <span class="block text-xs text-gray-500"
+              >Archipelagic Country found on the western part of the Pacific
+              Ocean</span
+            >
+          </div>
+          <span class="text-xs bg-green-300 rounded-full px-4 py-2">Join</span>
+        </li>
+        <li class="flex items-center space-x-2">
+          <img
+            class="inline-block h-10 w-10 rounded-full"
+            src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png"
+            alt=""
+          />
+          <div class="flex-grow">
+            <span class="block">Philippines</span>
+            <span class="block text-xs text-gray-500"
+              >Archipelagic Country found on the western part of the Pacific
+              Ocean</span
+            >
+          </div>
+          <span class="text-xs bg-green-300 rounded-full px-4 py-2">Join</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
